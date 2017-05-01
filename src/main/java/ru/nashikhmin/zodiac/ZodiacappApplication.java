20638ff3bcb70522172dfae514cb7b824df6a6b2
nashikhmin <veoring@gmail.com>
2017-05-01 11:34:44 +0500

    Added HelloWorld example

---
+++ src/main/java/ru/nashikhmin/zodiac/ZodiacappApplication.java
@@ -0,0 +1,12 @@
+package ru.nashikhmin.zodiac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZodiacappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZodiacappApplication.class, args);
+	}
+}
