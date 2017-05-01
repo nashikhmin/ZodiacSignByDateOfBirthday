9e03c5b446f89f9a5ce244f91079b153c569c064
nashikhmin <veoring@gmail.com>
2017-05-01 11:47:55 +0500

    created index files

---
+++ src/main/java/ru/nashikhmin/zodiac/controller/IndexController.java
@@ -0,0 +1,16 @@
+package ru.nashikhmin.zodiac.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
